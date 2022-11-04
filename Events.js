55aa5b6dbce86e65394b88562a45be4086738696
oceantwelvee <90316104+oceantwelvee@users.noreply.github.com>
2022-11-04 19:38:12 +0400

    Update Events.js

--- Events.js
+++ Events.js
@@ -1,3 +1,21 @@
 // DOM manipulation
 
-// Event Listeners;
+// Event Listeners
+
+const buttonTwo = document.querySelector(".btn2");
+
+function alertBtn() {
+    alert("I also love JavaScript");
+}
+
+buttonTwo.addEventListener("click", alertBtn);
+
+// Mouseover
+
+const newBackgroundColor = document.querySelector(".box3");
+
+function changeColor() {
+    newBackgroundColor.style.backgroundColor = "blue";
+}
+
+newBackgroundColor.addEventListener("mouseover", changeColor);
